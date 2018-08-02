2af9cda3135b999bf150ff96f0922b4aa4d78209
Danilo Silva <danilocgsilva@gmail.com>
2018-08-02 17:12:05 -0300

    Seemed to work, but outputed strange errors

--- mysqlchangeprefix.sh
+++ mysqlchangeprefix.sh
@@ -11,13 +11,68 @@ ask () {
   fi
 }
 
+ask_secret () {
+  read -s -p "$1" $2
+  echo ""
+  if [[ ${!2} = "" ]]
+  then
+    ask_secret "$1" $2
+    echo ""
+  fi
+}
+
 loop_through_tables_with_prefix () {
-  for i in $(mysql --login-path=$1 $2 -e "SHOW TABLES" | cat | sed 1d | sed -n /^$3/p)
+  loginpath=$1
+  databasename=$2
+  olddatabaseprefix=$3
+  newdatabaseprefix=$4
+  username=$5
+  password=$6
+
+  loop_tables $loginpath $databasename $username $password $olddatabaseprefix
+
+  for i in $(loop_tables $loginpath $databasename $username $password $olddatabaseprefix)
   do
+    oldtablename=$i
     new_table_name=$(get_name_name $i $3 $4)
-    mysql --login-path=$1 $2 -e "RENAME TABLE $i TO $new_table_name"
-    echo $i renomeada para $new_table_name
+    rename_table $loginpath $databasename $username $password $oldtablename
+    echo $i renamed to $new_table_name
   done
+}
+
+
+loop_tables () {
+
+  loginpath=$1
+  databasename=$2
+  username=$3
+  password=$4
+  olddatabaseprefix=$5
+
+  echo login_path = $loginpath
+
+  if [[ $loginpath != "-" ]]
+  then
+    mysql --login-path=$1 $databasename -e "SHOW TABLES" | cat | sed 1d | sed -n /^$olddatabaseprefix/p
+  else
+    mysql -u$username -p$password $databasename -e "SHOW TABLES" | cat | sed 1d | sed -n /^$olddatabaseprefix/p
+  fi
+}
+
+
+rename_table () {
+  loginpath=$1
+  databasename=$2
+  username=$3
+  password=$4
+  oldtablename=$5
+
+  if [ $loginpath != "-" ]
+  then
+    mysql --login-path=$loginpath $databasename -e "RENAME TABLE $oldtablename TO $new_table_name"
+  else
+    mysql -u$username -p$password $databasename -e "RENAME TABLE $oldtablename TO $new_table_name"
+  fi
 }
 
 get_name_name () {
@@ -26,16 +81,25 @@ get_name_name () {
 
 ## Main function
 mysqlchangeprefix () {
-  ask "Please, provides the login-path value: " login_path
+  read -p "Please, provides the login-path value: " login_path
   ask "Please, provides the database name: " database_name
   ask "Please, provides the old prefix: " database_old_prefix
   ask "Please, provides the new prefix: " database_new_prefix
+
+  if [[ $login_path = "" ]]
+  then
+    ask "Please, provides the database username: " username
+    ask_secret "Please, provides the database password: " password
+    login_path="-"
+  fi
 
   loop_through_tables_with_prefix \
     $login_path                   \
     $database_name                \
     $database_old_prefix          \
-    $database_new_prefix
+    $database_new_prefix          \
+    $username                     \
+    $password
 }
 
 ## detect if being sourced and
